1184dd0555808185f8162e98035adb5c4c335b83
Prasadasw <prasadaswar6870@gmail.com>
2024-08-27 10:43:56 +0530

    port change

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use('/', propertyRoutes);
 // app.use('/project', projectRoutes);
 
 // Define the port
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8001;
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
