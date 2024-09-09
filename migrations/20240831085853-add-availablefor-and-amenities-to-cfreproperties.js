0e29dfa8e9093d8e0ff9fdd285c4219f18828d5b
Prasadasw <prasadaswar6870@gmail.com>
2024-09-09 12:09:20 +0530

    removed available for

--- migrations/20240831085853-add-availablefor-and-amenities-to-cfreproperties.js
+++ migrations/20240831085853-add-availablefor-and-amenities-to-cfreproperties.js
@@ -1,15 +1,15 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.addColumn('CfreProperties', 'availableFor', {
-      type: Sequelize.STRING,
-    });
+    // await queryInterface.addColumn('CfreProperties', 'availableFor', {
+    //   type: Sequelize.STRING,
+    // });
     await queryInterface.addColumn('CfreProperties', 'amenities', {
       type: Sequelize.STRING,
     });
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.removeColumn('CfreProperties', 'availableFor');
+    // await queryInterface.removeColumn('CfreProperties', 'availableFor');
     await queryInterface.removeColumn('CfreProperties', 'amenities');
   },
 };
